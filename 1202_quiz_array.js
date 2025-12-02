c9f1e7152c0d44f1e9bb937dbbe11b1b4ce0608c
blackcrown <eunwoosunny@gmail.com>
2025-12-02 11:23:26 +0900

    1201_quiz update

---
+++ 1202_quiz_array.js
@@ -0,0 +1,15 @@
+const numbers = [5, 2, 11, 1, 25, 9, 32, 10];
+// 1. numbers 배열에서 짝수만 추출하시오
+// 2. numbers 배열에서 각 요소를 제곱한 새 배열을 만드시오
+// 3. numbers 배열을 내림차순으로 정렬하시오
+// 4. numbers 에 숫자 9가 포함되었는지 확인하시오
+// 5. numbers 배열에서 10보다 작은 값만 2배로 만든 새 배열을 만드시오
+
+// 6. 친구이름중에 'Camel'이 있는지 확인하는 함수 만들기
+
+const friends = ["Irangi", "Camel", "Shoopa"];
+function findMyFriend(names) {
+  //여기에 코드를 작성하세요
+}
+
+console.log(solution(friends)); // true
