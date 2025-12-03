eadc5c1ce3b618b031f00cc4ddecd76447da28f9
blackcrown <eunwoosunny@gmail.com>
2025-12-03 11:27:43 +0900

    my Task, object method

---
+++ 1203_02.js
@@ -0,0 +1,30 @@
+const person = {
+  name: "ghost",
+  age: 500,
+  mbti: null,
+};
+
+console.log(Object.keys(person).length);
+
+// person에 있는 값(value)들 중에, null이 있는지 확인해 보자.
+// 있으면 true, 없으면 false
+// includes() 메서드 사용.
+
+const hasNull = Object.values(person).includes(null);
+console.log(hasNull);
+
+const tasks = [
+  { id: 1, text: "walk" },
+  { id: 2, text: "running" },
+];
+
+const newTask = { id: 3, text: "programming" };
+
+const finalTasks = [...tasks, newTask];
+console.log(finalTasks);
+
+const tasks2 = [1, 2, 5];
+const newTasks2 = 3;
+const finalTasks2 = [...tasks2, newTasks2];
+
+console.log(finalTasks2);
