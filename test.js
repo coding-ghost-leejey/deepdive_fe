48189c8c639d09bfa33311d9d50abb4786373861
blackcrown <eunwoosunny@gmail.com>
2025-12-08 11:32:27 +0900

    reduce, timer함수

--- test.js
+++ test.js
@@ -1,12 +1,18 @@
-/* 영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다. 
-문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return하도록 solution 함수를 완성하세요.
-*/
+const cart = [
+  { menu: "latte", price: 3000, quantity: 1, stock: 15 },
+  { menu: "capuchino", price: 4500, quantity: 2, stock: 5 },
+  { menu: "Iced Americano", price: 2500, quantity: 1, stock: 8 },
+  { menu: "Orange Ade", price: 3200, quantity: 2, stock: 20 },
+];
 
-function solution(my_string) {
-  const newData = ["a", "e", "i", "o", "u"];
-  return my_string
-    .split("")
-    .filter((word) => !newData.includes(word))
-    .join("");
+function mission10(cart) {
+  let total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    const item = cart[i];
+    const price = item.stock < 10 ? item.price * 0.8 : item.price;
+    total += price * item.quantity;
+  }
+  return total;
 }
-console.log(solution("beautiful")); // 'btfl'
+
+console.log(mission10(cart));
